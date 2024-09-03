adbf9c6e29a5c80276d4ffe2e8da25b2e23abf76
narpatmiracle <128043832+narpatmiracle@users.noreply.github.com>
2024-09-03 17:04:39 +0530

    first commit

---
+++ src/ContactFromServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Narpat\Contactform;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContactFromServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        
+    }
+}
